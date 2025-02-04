e6fe9d66096308d7fa8284162c5794a9f72b20c7
Gustavo <gustavo.prog0@gmail.com>
2025-02-04 20:20:03 -0300

    feat/new repositories and interfaces

--- OpenSauce.Infrastructure/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ OpenSauce.Infrastructure/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e643be4ebc6f341f48201ece38fddfab1959e000")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f8d08579a2d51e73e904dc8de3cb60a521f8e2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
